0e0cf110e7478b1cd4cb4d10eb73e056a61cb2e4
zhangwenze1 <owen008@zju.edu.cn>
2015-08-24 20:43:46 +0800

    just add a markdown template

---
+++ leet_ Best Time to Buy and Sell Stock III/leet_ Best Time to Buy and Sell Stock III/main.cpp
@@ -0,0 +1,58 @@
+//
+//  main.cpp
+//  leet_ Best Time to Buy and Sell Stock III
+//
+//  Created by zhangwenze on 15/8/24.
+//  Copyright (c) 2015年 zhangwenze. All rights reserved.
+//
+
+
+/**
+ 
+ Best Time to Buy and Sell Stock III Total Accepted: 38468 Total Submissions: 160251 My Submissions Question Solution
+ Say you have an array for which the ith element is the price of a given stock on day i.
+ 
+ Design an algorithm to find the maximum profit. You may complete at most two transactions.
+ 
+ Note:
+ You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#include<string.h>
+#include<string>
+#include<ctype.h>
+
+#include<algorithm>
+#include<math.h>//有cmath 但是没有math
+
+#include<stack>
+#include<queue>
+#include<vector>// add graghs
+#include<map>
+
+#include<time.h>
+using namespace std;
+
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        return 0;
+    };
+};
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    vector<int> list(10,1);
+    list[1]=8;
+    
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ leet_ Best Time to Buy and Sell Stock III/leet_ Best Time to Buy and Sell Stock III/solution1.md
@@ -0,0 +1,2 @@
+下面是第一种方法 
+用到了 之前的Best Time to Buy and Sell Stock 1 中的方法
